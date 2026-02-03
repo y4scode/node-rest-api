cc667b3f9326dc6c69cf141049086ca3112dea2b
Yasmim Paiva <yasmimpaiva0303@gmail.com>
2026-02-03 11:45:33 -0300

    feat: add structured  message in json

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('node:http');
 
-http.createServer(function () {
-    console.log('The server was called.');
+http.createServer(function (request, response) {
+    response.writeHead(200, {'content-type': 'application/json; charset=utf-8'});
+    response.end(JSON.stringify({message: 'Hello Client!'}));
 }).listen(3000);
